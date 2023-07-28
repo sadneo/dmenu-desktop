aee1560c025e122300761dc5d38a09a9c1b3c1fa
sadneo <haywardeel@gmail.com>
2023-07-28 14:48:09 -0400

    rethought

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::{env, fs};
+use std::{env, fs, path};
 
+#[derive(Debug)]
 struct DesktopEntry {}
 
 fn main() {
     // clap
     // collect all the desktop entries and create a vector of structs with them
     let _entries = read_entries();
+    // clear up the entries
     // run dmenu and wait for the output
     // when dmenu returns, run the command in the struct
 }
@@ -28,29 +30,39 @@ fn read_entries() -> Option<Vec<DesktopEntry>> {
         application_folders.push(dir);
     }
     application_folders.insert(0, home_applications);
+    println!("{:?}", application_folders);
 
-    let entries = Vec::new();
+    let mut entries = Vec::new();
     for application_folder in application_folders {
-        entries.append(get_entries(application_folder));
+        let mut new_entries = get_entries(application_folder);
+        entries.append(&mut new_entries);
     }
 
     Some(entries)
 }
 
-fn get_entries<P: AsRef<Path>>(path: P) -> Vec<DesktopEntry> {
-    let entries = Vec::new();
-    let Ok(applications) = fs::read_dir(application_folder) else { continue };
-    for desktop_entry in applications {
-        let Ok(desktop_entry) = desktop_entry else { continue };
-        println!("{:?}", desktop_entry);
-        if desktop_entry.file_type().is_dir() {
-            entries.append(get_entries(desktop_entry.path()));
-        } else if desktop_entry.file_type().is_file() {
-            // create desktop entry
-            let entry;
-            entries.push(entry);
-        } else {
-            continue;
-        }
+fn get_entries<P: AsRef<path::Path>>(path: P) -> Vec<DesktopEntry> {
+    let entries: Vec<DesktopEntry> = Vec::new();
+    let Ok(applications) = fs::read_dir(path) else { return entries };
+    for entry in applications {
+        let Ok(entry) = entry else { continue };
+        println!("{:?}", entry);
+        let entry = parse_entry(entry.path());
+        println!("{:?}", entry);
     }
+    entries
+}
+
+fn parse_entry(entry: path::PathBuf) -> Option<DesktopEntry> {
+    let Ok(contents) = fs::read_to_string(entry) else { return None };
+    let mut split = contents.split("\n");
+    split.next(); // don't use the first line [Desktop Entry]
+    for line in split {
+        let mut line = line.split("=");
+        let key = line.nth(0);
+        let value = line.nth(1);
+        
+    }
+    // DesktopEntry.new(name, exec);
+    todo!();
 }
