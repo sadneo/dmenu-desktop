5e04e2808253496964c179912eca0a084e1622cf
sadneo <haywardeel@gmail.com>
2023-08-05 09:32:00 -0400

    Implemented clean_entries()

--- src/main.rs
+++ src/main.rs
@@ -30,23 +30,24 @@ struct DesktopEntry {
 }
 
 impl DesktopEntry {
-    fn new(name: String, hash_map: HashMap<String, String>) -> Option<DesktopEntry> {
+    fn new(hash_map: HashMap<String, String>) -> Option<DesktopEntry> {
         let nothing = String::from("");
 
+        let Some(name) = hash_map.get(&String::from("Name")) else { return None };
         let Some(exec) = hash_map.get(&String::from("Exec")) else { return None };
         let no_display = hash_map.get(&String::from("NoDisplay")).unwrap_or(&nothing);
         let hidden = hash_map.get(&String::from("Hidden")).unwrap_or(&nothing);
-        // let try_exec = hash_map.get(&String::from("TryExec"));
+        let try_exec = hash_map.get(&String::from("TryExec"));
         let path = hash_map.get(&String::from("Path"));
         let terminal = hash_map.get(&String::from("Terminal"));
 
         // test if tryexec is here, else don't make visible / skip entry
-        // if let Some(try_exec) = try_exec {
-        //     let path = Path::new(try_exec);
-        //     if !path.exists() {
-                
-        //     }
-        // }
+        if let Some(try_exec) = try_exec {
+            let path = Path::new(try_exec);
+            if !path.exists() {
+                return None;
+            }
+        }
 
         let hide = no_display == "true" || hidden == "true";
         let mut exec = exec.split(" ");
@@ -66,22 +67,44 @@ impl DesktopEntry {
             }
         }
 
-        Some(DesktopEntry { name, hide, command })
+        Some(DesktopEntry { name: name.to_owned(), hide, command })
     }
+}
+
+#[derive(Debug)]
+enum ErrorKind {
+    HomeNotFound,
 }
 
 fn main() {
     let cli = Cli::parse();
-    let entries = read_entries();
-    // clean_entries(&mut entries);
-    // run dmenu and wait for the output
-    // when dmenu returns, run the command in the struct
-    // use usage_log
+    let entries = match read_entries() {
+        Ok(t) => t,
+        Err(error_kind) => match error_kind {
+            ErrorKind::HomeNotFound => {
+                eprintln!("$HOME not found");
+                return;
+            }
+        }
+    };
+
+    let cleaned_entries = clean_entries(entries);
+    // 2: sort using usage_log
+
+    if let Some(dmenu) = cli.dmenu {
+        // run dmenu and wait for the output
+        // when dmenu returns, run the command in the struct
+        // 2: update usage_log
+    } else {
+        // print to stdout
+    }
 }
 
-fn read_entries() -> Result<Vec<DesktopEntry>, env::VarError> {
-    let xdg_data_dirs = env::var("XDG_DATA_DIRS")?; // TODO: use defaults if not present.
-    let mut home_applications = env::var("HOME")?;
+fn read_entries() -> Result<Vec<DesktopEntry>, ErrorKind> {
+    let Ok(mut home_applications) = env::var("HOME") else {
+        return Err(ErrorKind::HomeNotFound);
+    };
+    let xdg_data_dirs = env::var("XDG_DATA_DIRS").unwrap_or(String::new());
     home_applications.push_str("/.local/share/applications");
 
     let data_dirs = xdg_data_dirs.split(":").collect::<Vec<&str>>();
@@ -103,8 +126,17 @@ fn read_entries() -> Result<Vec<DesktopEntry>, env::VarError> {
     Ok(entries)
 }
 
-fn clean_entries(entries: &mut Vec<DesktopEntry>) {
-    // TODO: clean repeats, and remove everything that's hidden
+fn clean_entries(entries: Vec<DesktopEntry>) -> Vec<DesktopEntry> {
+    let mut new_entries: Vec<DesktopEntry> = Vec::new();
+    for entry in entries {
+        if entry.hide {
+            new_entries.retain(|x| x.name != entry.name);
+            continue;
+        }
+        new_entries.push(entry);
+    }
+
+    new_entries
 }
 
 fn get_entries<P: AsRef<Path>>(path: P) -> Vec<DesktopEntry> {
@@ -141,7 +173,6 @@ fn parse_entry(entry: PathBuf) -> Option<DesktopEntry> {
     // check if type is application
     let Some(kind) = hash_map.remove(&String::from("Type")) else { return None };
     if kind != "Application" { return None }
-    let Some(name) = hash_map.remove(&String::from("Name")) else { return None };
     
-    DesktopEntry::new(name, hash_map)
+    DesktopEntry::new(hash_map)
 }
