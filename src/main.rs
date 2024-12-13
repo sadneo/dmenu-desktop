6d80d8eac0fe638e9dba09e6021904b79e9d2c45
sadneo <haywardeel@gmail.com>
2024-12-13 15:11:41 -0500

    chore: formatted

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::{env, fs};
+use std::io::{self, Write};
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
-use std::io::{self, Write};
+use std::{env, fs};
 
 use clap::{Parser, ValueEnum};
 use ini::Ini;
@@ -16,22 +16,25 @@ enum EntryType {
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    #[arg(long, default_value="name")]
+    #[arg(long, default_value = "name")]
     entry_type: EntryType,
-
-    /// Determines the command used to invoke dmenu or an equivalent. Executed with your shell ($SHELL) or /bin/sh
-    #[arg(long)]
-    dmenu: Option<String>,
 
     /// Must point to a read-writeable file (will create if not exists). In this mode entries are sorted by usage frequency.
     #[arg(long)]
     usage_log: Option<PathBuf>,
 
+    /// Determines the command used to invoke dmenu or an equivalent.
+    #[arg(long)]
+    dmenu: Option<String>,
+
     /// Terminal emulator used to launch applications, does nothing if dmenu is not provided, put {} where the dmenu command should go
     #[arg(long)]
     terminal: Option<String>,
-}
 
+    /// Which shell to execute commands with, defaults to $SHELL then /bin/sh
+    #[arg(long)]
+    shell: Option<String>,
+}
 
 #[derive(Debug)]
 struct DesktopEntry {
@@ -45,11 +48,13 @@ struct DesktopEntry {
 
 impl DesktopEntry {
     fn from_ini(filename: &str, ini: Ini) -> Option<DesktopEntry> {
-        let Some(section) = ini.section(Some("Desktop Entry")) else { return None };
-        if section.get("Type") != Some("Application") { return None; }
-        
-        let Some(name) = section.get("Name") else { return None };
-        let Some(exec) = section.get("Exec") else { return None };
+        let section = ini.section(Some("Desktop Entry"))?;
+        if section.get("Type") != Some("Application") {
+            return None;
+        }
+
+        let name = section.get("Name")?;
+        let exec = section.get("Exec")?;
 
         let try_exec = section.get("TryExec").map(str::to_string);
         let path = section.get("Path").map(str::to_string);
@@ -63,16 +68,18 @@ impl DesktopEntry {
             }
         }
 
-        let hide = !exec_exists || section.get("NoDisplay") == Some("true") || section.get("Hidden") == Some("true");
-        
+        let hide = !exec_exists
+            || section.get("NoDisplay") == Some("true")
+            || section.get("Hidden") == Some("true");
+
         Some(DesktopEntry {
             name: name.to_owned(),
             filename: filename.to_owned(),
             exec: exec.to_owned(),
             hide,
             terminal,
-            path
-        } )
+            path,
+        })
     }
     fn field(&self, entry_type: &EntryType) -> &str {
         match entry_type {
@@ -84,7 +91,9 @@ impl DesktopEntry {
 }
 
 fn exists_on_path(exec: &str) -> bool {
-    let Ok(path_var) = env::var("PATH") else { return false };
+    let Ok(path_var) = env::var("PATH") else {
+        return false;
+    };
     let path_var = env::split_paths(&path_var);
     for dir in path_var {
         let test_path = dir.join(exec);
@@ -106,50 +115,79 @@ fn main() -> std::io::Result<()> {
         if entry.hide {
             hidden_entries.push(entry);
             continue;
-        } else if hidden_entries.iter().find(|e| e.name == entry.name).is_none() {
+        } else if hidden_entries.iter().any(|e| e.name == entry.name) {
             entries_string.push_str(entry.field(&cli.entry_type));
-            entries_string.push_str("\n");
+            entries_string.push('\n');
         }
     }
 
-    if let Some(dmenu) = cli.dmenu {
-        let Some(mut dmenu_split) = shlex::split(&dmenu) else { return Err(io::Error::new(io::ErrorKind::Other, "Invalid dmenu command.")) };
-        let program = dmenu_split.remove(0);
-        let mut menu_handle = Command::new(program).args(dmenu_split).stdin(Stdio::piped()).stdout(Stdio::piped()).spawn()?;
-        let _ = menu_handle.stdin.as_mut().unwrap().write(entries_string.as_bytes());
-        let output = String::from_utf8(menu_handle.wait_with_output()?.stdout).expect("Output should be valid UTF8");
-
-        let Some(selected_entry) = entries.iter().find(|e| e.field(&cli.entry_type) == output.trim()) else {
-            let Some(mut split) = shlex::split(output.trim()) else { return Err(io::Error::new(io::ErrorKind::Other, "Invalid command.")) };
-            let program = split.remove(0);
-            let output = Command::new(program).args(split).output()?;
-            println!("{}", String::from_utf8_lossy(&output.stdout));
-            eprintln!("Command exited with status {}", output.status.code().unwrap_or(-1));
-            return Ok(());
-        };
-        
-        let mut command_string = selected_entry.exec.to_owned();
-        if cli.terminal.is_some() && selected_entry.terminal {
-            let terminal = cli.terminal.unwrap();
-            if !terminal.contains("{}") {
-                return Err(io::Error::new(io::ErrorKind::Other, "Invalid terminal command"));
-            }
-            command_string = terminal.replace("{}", command_string.as_str());
-        }
-
-        let Some(mut exec_split) = shlex::split(command_string.as_str()) else { return Err(io::Error::new(io::ErrorKind::Other, "Invalid exec key.")) };
-        let program = exec_split.remove(0);
-        let mut command = Command::new(program);
-        command.args(exec_split);
-        if let Some(path) = &selected_entry.path {
-            command.current_dir(path);
-        }
-
-        if let Err(e) = command.spawn() {
-            eprintln!("Application exited with error: {}", e);
-        }
-    } else {
+    if cli.dmenu.is_none() {
         print!("{}", entries_string);
+        return Ok(());
+    }
+
+    let dmenu = cli.dmenu.unwrap();
+    let Some(mut dmenu_split) = shlex::split(&dmenu) else {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            "Invalid dmenu command.",
+        ));
+    };
+    let program = dmenu_split.remove(0);
+    let mut menu_handle = Command::new(program)
+        .args(dmenu_split)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+    let _ = menu_handle
+        .stdin
+        .as_mut()
+        .unwrap()
+        .write(entries_string.as_bytes());
+    let output = String::from_utf8(menu_handle.wait_with_output()?.stdout)
+        .expect("Output should be valid UTF8");
+
+    let Some(selected_entry) = entries
+        .iter()
+        .find(|e| e.field(&cli.entry_type) == output.trim())
+    else {
+        let Some(mut split) = shlex::split(output.trim()) else {
+            return Err(io::Error::new(io::ErrorKind::Other, "Invalid command."));
+        };
+        let program = split.remove(0);
+        let output = Command::new(program).args(split).output()?;
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+        eprintln!(
+            "Command exited with status {}",
+            output.status.code().unwrap_or(-1)
+        );
+        return Ok(());
+    };
+
+    let mut command_string = selected_entry.exec.to_owned();
+    if cli.terminal.is_some() && selected_entry.terminal {
+        let terminal = cli.terminal.unwrap();
+        if !terminal.contains("{}") {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Invalid terminal command",
+            ));
+        }
+        command_string = terminal.replace("{}", command_string.as_str());
+    }
+
+    let Some(mut exec_split) = shlex::split(command_string.as_str()) else {
+        return Err(io::Error::new(io::ErrorKind::Other, "Invalid exec key."));
+    };
+    let program = exec_split.remove(0);
+    let mut command = Command::new(program);
+    command.args(exec_split);
+    if let Some(path) = &selected_entry.path {
+        command.current_dir(path);
+    }
+
+    if let Err(e) = command.spawn() {
+        eprintln!("Application exited with error: {}", e);
     }
     Ok(())
 }
@@ -160,11 +198,14 @@ fn read_entries() -> io::Result<Vec<DesktopEntry>> {
         None => match env::var_os("HOME") {
             Some(home) => PathBuf::from(home).join(".local/share/applications"),
             None => return Err(io::Error::new(io::ErrorKind::Other, "HomeNotFound")),
-        }
+        },
     };
     let data_dirs = match env::var_os("XDG_DATA_DIRS") {
         Some(dirs) => env::split_paths(&dirs).map(PathBuf::from).collect(),
-        None => vec![PathBuf::from("/usr/local/share"), PathBuf::from("/usr/share")],
+        None => vec![
+            PathBuf::from("/usr/local/share"),
+            PathBuf::from("/usr/share"),
+        ],
     };
 
     let mut application_dirs = Vec::new();
@@ -184,22 +225,37 @@ fn read_entries() -> io::Result<Vec<DesktopEntry>> {
 
 fn get_entries<P: AsRef<Path>>(path: P) -> Vec<DesktopEntry> {
     let mut entries: Vec<DesktopEntry> = Vec::new();
-    let Ok(applications) = fs::read_dir(path) else { return entries };
+    let Ok(applications) = fs::read_dir(path) else {
+        return entries;
+    };
     for file in applications {
         let Ok(file) = file else { continue };
 
         let path = file.path();
-        let Some(stem) = path.file_stem() else { continue; };
-        let Some(stem) = stem.to_str() else { continue; };
+        let Some(stem) = path.file_stem() else {
+            continue;
+        };
+        let Some(stem) = stem.to_str() else {
+            continue;
+        };
         let extension = path.extension();
-        if extension == None || extension.unwrap() != "desktop" {
+        if extension.is_none() || extension.unwrap() != "desktop" {
             continue;
         }
 
-        let Ok(ini) = Ini::load_from_file_opt(&path, ini::ParseOption { enabled_quote: false, enabled_escape: false } ) else { continue };
-        let Some(entry) = DesktopEntry::from_ini(stem, ini) else { continue };
+        let Ok(ini) = Ini::load_from_file_opt(
+            &path,
+            ini::ParseOption {
+                enabled_quote: false,
+                enabled_escape: false,
+            },
+        ) else {
+            continue;
+        };
+        let Some(entry) = DesktopEntry::from_ini(stem, ini) else {
+            continue;
+        };
         entries.push(entry);
     }
     entries
 }
-
